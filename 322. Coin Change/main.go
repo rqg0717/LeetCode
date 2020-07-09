d08293d3f5bcb7c76cb539fa3d4bb5d5d41a3184
James Ren <rqg0717@gmail.com>
2020-07-08 22:14:02 -0500

    322 improved performance

--- 322. Coin Change/main.go
+++ 322. Coin Change/main.go
@@ -15,14 +15,11 @@ func min(x, y int) int {
 	---> dp(amount) = min(dp(amount - coins[len(coins)])) + 1
 */
 func coinChange(coins []int, amount int) int {
-	dp := make(map[int]int)
-	for i := 0; i < amount+1; i++ {
-		dp[i] = amount + 1
-	}
+	dp := make([]int, amount+1)
 
 	dp[0] = 0
-
 	for i := 1; i <= amount; i++ {
+		dp[i] = amount + 1
 		for j := 0; j < len(coins); j++ {
 			if i >= coins[j] {
 				dp[i] = min(dp[i-coins[j]]+1, dp[i])
@@ -39,6 +36,6 @@ func coinChange(coins []int, amount int) int {
 
 func main() {
 	coins := []int{1, 5, 25, 100}
-	amount := 200
+	amount := 113
 	fmt.Println("output: ", coinChange(coins, amount))
 }
