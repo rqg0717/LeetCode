3cc3e35ec2277dd0f4be7335bc9ac155c47ac08c
James Ren <rqg0717@gmail.com>
2020-07-08 21:44:34 -0500

    Added 322. Coin Change

---
+++ 322. Coin Change/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func min(x, y int) int {
+	if x > y {
+		return y
+	}
+	return x
+}
+
+/*
+	e.g. coins = [1, 2, 5], amount = 11
+	then dp(11) = min(dp(11 - 1), dp(11 - 2), dp(11 - 5)) + 1
+	---> dp(amount) = min(dp(amount - coins[len(coins)])) + 1
+*/
+func coinChange(coins []int, amount int) int {
+	dp := make(map[int]int)
+	for i := 0; i < amount+1; i++ {
+		dp[i] = amount + 1
+	}
+
+	dp[0] = 0
+
+	for i := 1; i <= amount; i++ {
+		for j := 0; j < len(coins); j++ {
+			if i >= coins[j] {
+				dp[i] = min(dp[i-coins[j]]+1, dp[i])
+			}
+		}
+	}
+
+	if dp[amount] > amount {
+		return -1
+	}
+
+	return dp[amount]
+}
+
+func main() {
+	coins := []int{1, 5, 25, 100}
+	amount := 200
+	fmt.Println("output: ", coinChange(coins, amount))
+}
