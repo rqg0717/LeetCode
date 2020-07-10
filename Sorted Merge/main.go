ba99c7f44075b81e9a0cbf25c443bcd361b0e900
James Ren <rqg0717@gmail.com>
2020-07-09 23:32:27 -0500

    Added sorted merge

--- Sorted Merge/main.go
+++ Sorted Merge/main.go
@@ -10,11 +10,32 @@ func merge(A []int, m int, B []int, n int) {
 	sort.Ints(A)
 }
 
+func merge1(A []int, m int, B []int, n int) {
+	indexA := m - 1
+	indexB := n - 1
+	index := len(A) - 1
+	for indexA >= 0 && indexB >= 0 {
+		if A[indexA] > B[indexB] {
+			A[index] = A[indexA]
+			indexA--
+		} else {
+			A[index] = B[indexB]
+			indexB--
+		}
+		index--
+	}
+	for indexB >= 0 {
+		A[index] = B[indexB]
+		indexB--
+		index--
+	}
+}
+
 func main() {
 	A := []int{1, 2, 3, 0, 0, 0}
 	m := 3
 	B := []int{2, 5, 6}
 	n := 3
-	merge(A, m, B, n)
+	merge1(A, m, B, n)
 	fmt.Println("Output: ", A)
 }
