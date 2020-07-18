38fc35c11cd3eba4a57004ba13c6e273d9ded330
James Ren <rqg0717@gmail.com>
2020-07-18 18:04:01 -0500

    Updated 121 and 122.

--- Stock/121. Best Time to Buy and Sell Stock/main.go
+++ Stock/121. Best Time to Buy and Sell Stock/main.go
@@ -25,8 +25,30 @@ func maxProfit(prices []int) int {
 	return profit
 }
 
+// dynamic programming
+// dp[i][j]
+// for i <= n {
+//  for j <= 1 {
+//   dp[i][j] = max(buy, sell)
+//  }
+// }
+func maxProfit1(prices []int) int {
+	if len(prices) == 0 {
+		return 0
+	}
+	dp0 := 0
+	dp1 := -prices[0]
+	for i := 0; i < len(prices); i++ {
+		// today, do not have any stock
+		dp0 = max(dp0, dp1+prices[i]) // yesterday did not have/sold today
+		// today, have stock
+		dp1 = max(dp1, -prices[i]) // yesterday had stock/bought today
+	}
+	return dp0
+}
+
 func main() {
 	prices := make([]int, 0)
 	prices = append(prices, 7, 1, 5, 3, 6, 4)
-	fmt.Println("max profit: ", maxProfit(prices))
+	fmt.Println("max profit: ", maxProfit1(prices))
 }
--- Stock/122. Best Time to Buy and Sell Stock II/main.go
+++ Stock/122. Best Time to Buy and Sell Stock II/main.go
@@ -9,18 +9,27 @@ func max(x, y int) int {
 	return y
 }
 
+// dynamic programming
+// dp[i][j]
+// for i <= n {
+//  for j <= 1 {
+//   dp[i][j] = max(buy, sell)
+//  }
+// }
 func maxProfit(prices []int) int {
-	if len(prices) < 2 {
+	if len(prices) == 0 {
 		return 0
 	}
-	dp := make([][2]int, len(prices))
-	dp[0][0] = 0
-	dp[0][1] = -prices[0]
-	for i := 1; i < len(prices); i++ {
-		dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i])
-		dp[i][1] = max(dp[i-1][0]-prices[i], dp[i-1][1])
+	dp0 := 0
+	dp1 := -prices[0]
+	for i := 0; i < len(prices); i++ {
+		temp := dp0
+		// today, do not have any stock
+		dp0 = max(dp0, dp1+prices[i]) // yesterday did not have/sold today
+		// today, have stock
+		dp1 = max(dp1, temp-prices[i]) // yesterday had stock/bought today
 	}
-	return dp[len(prices)-1][0]
+	return dp0
 }
 
 func main() {
