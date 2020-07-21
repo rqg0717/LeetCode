1d6412b4c4ead9e89989f6f8cbb47be48432cad3
James Ren <rqg0717@gmail.com>
2020-07-20 20:01:54 -0500

    Added 198.

---
+++ House Robber/198. House Robber/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+// dynamic programming
+// dp[i] the maximum amount of money of i rooms
+// for i <= n {
+//   dp[i]= max(dp[i-1], dp[i-2]+nums[i])
+// }
+func rob(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	dp0, dp1, dp2 := 0, 0, 0
+	for i := 0; i < len(nums); i++ {
+
+		dp0 = max(dp1, dp2+nums[i])
+		dp2 = dp1
+		dp1 = dp0
+	}
+	return dp0
+}
+
+func main() {
+	nums := make([]int, 0)
+	nums = append(nums, 2, 7, 9, 3, 1)
+	fmt.Println("max profit: ", rob(nums))
+}
