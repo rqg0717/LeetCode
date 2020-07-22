bb6459b812c37ea03ea5f46d3207e8e759c4bdb1
James Ren <rqg0717@gmail.com>
2020-07-21 19:10:45 -0500

    Updated 337.

--- House Robber/337. House Robber III/main.go
+++ House Robber/337. House Robber III/main.go
@@ -11,8 +11,29 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func rob(root *TreeNode) int {
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
 
+// not rob = 0; rob = 1
+// rob root: root:1 + left:0  + right:0
+// not rob root: root:0 & max(left:0, left:1) + max(right:0, right:1)
+func robNodes(root *TreeNode) (int, int) {
+	if root == nil {
+		return 0, 0
+	}
+	left1, left0 := robNodes(root.Left)
+	right1, right0 := robNodes(root.Right)
+	return root.Val + left0 + right0,
+		max(left0, left1) + max(right0, right1)
+}
+
+func rob(root *TreeNode) int {
+	root1, root0 := robNodes(root)
+	return max(root0, root1)
 }
 
 func main() {
@@ -28,7 +49,7 @@ func main() {
 	root.Left.Right = node3
 	root.Right.Right = node4
 
-	fmt.Println("input: ", *root, *node1, *node2, *node3, *node4)
+	//fmt.Println("input: ", *root, *node1, *node2, *node3, *node4)
 
-	fmt.Println("max profit: ", rob(root))
+	fmt.Println("Maximum amount of money the thief can rob: ", rob(root))
 }
