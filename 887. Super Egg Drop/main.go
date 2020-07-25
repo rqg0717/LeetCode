2e8f5de9a3e7cb92fd206113cb494c89c45681bd
James Ren <rqg0717@gmail.com>
2020-07-25 13:46:53 -0500

    Updated 887.

--- 887. Super Egg Drop/main.go
+++ 887. Super Egg Drop/main.go
@@ -6,6 +6,12 @@ import "fmt"
 // not broken dp[k][n-1]
 // dp[k][n] = dp[k][n-1] + dp[k-1][n-1] + current floor
 func superEggDrop(K int, N int) int {
+	if K == 1 {
+		return N
+	}
+	if N == 0 {
+		return 0
+	}
 	dp := make([][]int, K+1)
 	for k := 0; k <= K; k++ {
 		dp[k] = make([]int, N+1)
@@ -21,13 +27,25 @@ func superEggDrop(K int, N int) int {
 	return N
 }
 
+// from https://leetcode.com/festival/
+func superEggDrop1(K int, N int) int {
+	var i int
+	dp := make([]int, K+1)
+	for i = 0; dp[K] < N; i++ {
+		for j := K; j > 0; j-- {
+			dp[j] += dp[j-1] + 1
+		}
+	}
+	return i
+}
+
 // set the minimum number of moves as m
 // broken: 1; not broken: 0
-func superEggDrop1(K int, N int) int {
+func superEggDrop2(K int, N int) int {
 	return 0
 }
 
 func main() {
-	K, N := 1, 2
+	K, N := 100, 10000
 	fmt.Println("the minimum number of moves: ", superEggDrop1(K, N))
 }
