98b5dbbe6223117cd491faa84f5d0f5ed77805e2
James Ren <rqg0717@gmail.com>
2020-08-09 12:12:23 -0500

    Updated 125.

--- Strings/125. Valid Palindrome/main.go
+++ Strings/125. Valid Palindrome/main.go
@@ -5,7 +5,7 @@ import "fmt"
 func isPalindrome(s string) bool {
 	var ss []rune
 	n := len(s)
-	if n == 0 {
+	if n <= 1 {
 		return true
 	}
 	// converts all characters to lower cases and removes symbols
@@ -27,7 +27,51 @@ func isPalindrome(s string) bool {
 	return true
 }
 
+// is not 0-9 a-z A-Z?
+func isNotChar(ch byte) bool {
+	if ch <= 47 || (ch >= 58 && ch <= 64) ||
+		(ch <= 96 && ch >= 91) || ch >= 123 {
+		return true
+	}
+	return false
+}
+
+func isPalindrome1(s string) bool {
+	n := len(s)
+	if n <= 1 {
+		return true
+	}
+	left := 0
+	right := n - 1
+	for left < right {
+		// skips symbols
+		if isNotChar(s[left]) {
+			left++
+			continue
+		}
+		if isNotChar(s[right]) {
+			right--
+			continue
+		}
+		leftChar := s[left]
+		rightChar := s[right]
+		// converts uppercase to lowercase
+		if 65 <= leftChar && leftChar <= 90 {
+			leftChar += 32
+		}
+		if 65 <= rightChar && rightChar <= 90 {
+			rightChar += 32
+		}
+		if leftChar != rightChar {
+			return false
+		}
+		left++
+		right--
+	}
+	return true
+}
+
 func main() {
 	s := "A man, a plan, a canal: Panama"
-	fmt.Println("Output: ", isPalindrome(s))
+	fmt.Println("Output: ", isPalindrome1(s))
 }
