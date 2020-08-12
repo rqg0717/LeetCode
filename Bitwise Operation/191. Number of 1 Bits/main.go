9dd5ddabff32df96e7eb2e8f22b45f1271b80dbc
James Ren <rqg0717@gmail.com>
2020-08-11 21:51:58 -0500

    Updated 191.

--- Bitwise Operation/191. Number of 1 Bits/main.go
+++ Bitwise Operation/191. Number of 1 Bits/main.go
@@ -13,7 +13,19 @@ func hammingWeight(num uint32) int {
 	return count
 }
 
+func hammingWeight1(num uint32) int {
+	count := 0
+	mask := uint32(1)
+	for i := 0; i < 32; i++ {
+		if (num & mask) != 0 {
+			count++
+		}
+		mask = mask << 1
+	}
+	return count
+}
+
 func main() {
 	num := uint32(0b11111111111111111111111111111101)
-	fmt.Println("Output: ", hammingWeight(num))
+	fmt.Println("Output: ", hammingWeight1(num))
 }
