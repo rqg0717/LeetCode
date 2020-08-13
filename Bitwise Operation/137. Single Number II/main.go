2958a7c7e5a92840a3e8412d792c3efdf70ff521
James Ren <rqg0717@gmail.com>
2020-08-12 23:08:11 -0500

    Updated 137.

--- Bitwise Operation/137. Single Number II/main.go
+++ Bitwise Operation/137. Single Number II/main.go
@@ -18,7 +18,17 @@ func singleNumber(nums []int) int {
 	return 0
 }
 
+func singleNumber1(nums []int) int {
+	a, b, c := 0, 0, 0
+	for _, num := range nums {
+		a = (b & ^num) | (c & num)
+		c = (^b & ^c & num) | (c & ^num)
+		b = a
+	}
+	return c
+}
+
 func main() {
 	nums := []int{0, 1, 0, 1, 0, 1, 99}
-	fmt.Println("Output: ", singleNumber(nums))
+	fmt.Println("Output:", singleNumber1(nums))
 }
