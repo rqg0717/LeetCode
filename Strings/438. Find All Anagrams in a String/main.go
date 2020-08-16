f835d14442cd64ea7ae4a7561e306a440674ed89
James Ren <rqg0717@gmail.com>
2020-08-16 12:27:02 -0500

    Updated 438.

--- Strings/438. Find All Anagrams in a String/main.go
+++ Strings/438. Find All Anagrams in a String/main.go
@@ -28,8 +28,48 @@ func findAnagrams(s string, p string) []int {
 	return indexs
 }
 
+func findAnagrams1(s string, p string) []int {
+	sn := len(s)
+	pn := len(p)
+	indexs := []int{}
+	if sn == 0 || pn == 0 || pn > sn {
+		return nil
+	}
+	window := make(map[byte]int)
+	pmap := make(map[byte]int)
+	for i := 0; i < pn; i++ {
+		pmap[p[i]]++
+	}
+	left, right := 0, 0
+	valid := 0
+	for right < len(s) {
+		c1 := s[right]
+		right++
+		if _, ok := pmap[c1]; ok {
+			window[c1]++
+			if window[c1] == pmap[c1] {
+				valid++
+			}
+		}
+		for right-left >= pn {
+			if valid == len(pmap) {
+				indexs = append(indexs, left)
+			}
+			c2 := s[left]
+			left++
+			if _, ok := window[c2]; ok {
+				if window[c2] == pmap[c2] {
+					valid--
+				}
+				window[c2]--
+			}
+		}
+	}
+	return indexs
+}
+
 func main() {
 	s := "ababb"
 	p := "ab"
-	fmt.Println("Output: ", findAnagrams(s, p))
+	fmt.Println("Output: ", findAnagrams1(s, p))
 }
