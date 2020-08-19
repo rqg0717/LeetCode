7291252831c81c5c82da0466d5a109eaafa75554
James Ren <rqg0717@gmail.com>
2020-08-18 19:57:57 -0500

    Added 350.

--- Arrays/349. Intersection of Two Arrays/main.go
+++ Arrays/349. Intersection of Two Arrays/main.go
@@ -40,8 +40,24 @@ func intersection1(nums1 []int, nums2 []int) []int {
 	return results
 }
 
+func intersection2(nums1 []int, nums2 []int) []int {
+	m := map[int]int{}
+	for _, value := range nums1 {
+		m[value]++
+	}
+	index := 0
+	for _, value := range nums2 {
+		if m[value] > 0 {
+			m[value] = 0
+			nums2[index] = value
+			index++
+		}
+	}
+	return nums2[0:index]
+}
+
 func main() {
 	nums1 := []int{1, 2, 2, 1}
 	nums2 := []int{2, 2}
-	fmt.Println("Output: ", intersection1(nums1, nums2))
+	fmt.Println("Output: ", intersection2(nums1, nums2))
 }
