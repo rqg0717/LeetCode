3bc4e7b3c4e615e3621287835a80bce61f5dcc7b
James Ren <rqg0717@gmail.com>
2020-08-18 19:22:33 -0500

    Added split string into prime numbers

---
+++ Strings/split string into prime numbers/README.md
@@ -0,0 +1,5 @@
+Given a string of length n consisting of digits [0-9], count the number of ways the given string can be split into prime numbers.
+Since the answer can be large, return the answer modulo 109 + 7. 
+Note: A partition that contains numbers with leading zeroes will be invalid and the initial string does not contain leading zeroes. 
+
+For example the input string to be s = "3175", then this string can be split into 3 different ways as (31, 7,5), (3, 17, 5), (317,5) where each one of them contains only prime numbers.
---
+++ Strings/split string into prime numbers/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func countPrimes(s string) int {
+	n := int(math.Pow10(len(s)))
+	// Sieve of Eratosthenes
+	isPrime := make([]bool, n)
+	for i := range isPrime {
+		isPrime[i] = true
+	}
+	for i := 2; i*i <= n; i++ {
+		if isPrime[i] {
+			for j := i * i; j < n; j += i {
+				isPrime[j] = false
+			}
+		}
+	}
+	l := len(s) + 1
+	dp := make([]int, l)
+	for i := 1; i < l; i++ {
+		count := 0
+		for j := 1; j < i; j++ {
+			index, _ := strconv.Atoi(s[j:i])
+			if index > n {
+				break
+			}
+			if index > 1 {
+				if isPrime[index] {
+					count++
+				}
+			}
+		}
+		dp[i] = count
+		index, _ := strconv.Atoi(s[:i])
+		if index > n {
+			dp[i] = max(dp[i-1], dp[i])
+			continue
+		}
+		if index > 1 {
+			if isPrime[index] {
+				dp[i]++
+			}
+		}
+		dp[i] = max(dp[i-1], dp[i])
+	}
+	return dp[l-1]
+}
+
+func main() {
+	s := "3175"
+	fmt.Println("output:", countPrimes(s))
+}
