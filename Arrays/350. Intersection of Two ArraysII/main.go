a13c86d0459f7498cc82be9df1b23d9a030b659b
James Ren <rqg0717@gmail.com>
2020-08-19 21:37:59 -0500

    Updated 350.

--- Arrays/350. Intersection of Two ArraysII/main.go
+++ Arrays/350. Intersection of Two ArraysII/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func intersect(nums1 []int, nums2 []int) []int {
@@ -35,8 +36,28 @@ func intersect1(nums1 []int, nums2 []int) []int {
 	return nums2[0:index]
 }
 
+// What if the given array is already sorted? How would you optimize your algorithm?
+func intersect2(nums1 []int, nums2 []int) []int {
+	sort.Ints(nums1)
+	sort.Ints(nums2)
+	i, j, k := 0, 0, 0
+	for i < len(nums1) && j < len(nums2) {
+		if nums1[i] == nums2[j] {
+			nums1[k] = nums1[i]
+			i++
+			j++
+			k++
+		} else if nums1[i] < nums2[j] {
+			i++
+		} else {
+			j++
+		}
+	}
+	return nums1[:k]
+}
+
 func main() {
 	nums1 := []int{4, 9, 5}
 	nums2 := []int{9, 4, 9, 8, 4}
-	fmt.Println("Output: ", intersect(nums1, nums2))
+	fmt.Println("Output: ", intersect2(nums1, nums2))
 }
