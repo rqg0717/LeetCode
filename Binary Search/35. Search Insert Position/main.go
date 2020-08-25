09333238f4fe65f85f905ff404b9a1fbcfd27991
James Ren <rqg0717@gmail.com>
2020-08-25 18:57:46 -0500

    Updated 35.

--- Binary Search/35. Search Insert Position/main.go
+++ Binary Search/35. Search Insert Position/main.go
@@ -18,9 +18,32 @@ func searchInsert(nums []int, target int) int {
 	return left
 }
 
+// #3
+func searchInsert1(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left+1 < right {
+		mid := left + (right-left)>>1
+		if nums[mid] == target {
+			right = mid
+		} else if nums[mid] > target {
+			right = mid
+		} else {
+			left = mid
+		}
+	}
+	if nums[left] >= target {
+		return left
+	} else if nums[right] >= target {
+		return right
+	} else if nums[right] < target {
+		return right + 1
+	}
+	return 0
+}
+
 func main() {
 	nums := []int{1, 3, 5, 6, 7, 9}
 	target := 2
 	fmt.Println("input Array: ", nums)
-	fmt.Println("Output: ", searchInsert(nums, target))
+	fmt.Println("Output: ", searchInsert1(nums, target))
 }
