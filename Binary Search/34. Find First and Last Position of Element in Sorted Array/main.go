381b00c0f79cbad83e27d955ca00c65e8b028921
James Ren <rqg0717@gmail.com>
2020-08-27 18:07:10 -0500

    Updated 34.

--- Binary Search/34. Find First and Last Position of Element in Sorted Array/main.go
+++ Binary Search/34. Find First and Last Position of Element in Sorted Array/main.go
@@ -38,9 +38,33 @@ func searchRange(nums []int, target int) []int {
 	return []int{searchFirst(nums, target), searchLast(nums, target)}
 }
 
+func findIndex(nums []int, target int, isLeft bool) int {
+	left, right := 0, len(nums)
+	for left < right {
+		mid := left + (right-left)>>1
+		if nums[mid] > target || (isLeft && target == nums[mid]) {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
+}
+
+func searchRange1(nums []int, target int) []int {
+	targetRange := []int{-1, -1}
+	left := findIndex(nums, target, true)
+	if left == len(nums) || nums[left] != target {
+		return targetRange
+	}
+	targetRange[0] = left
+	targetRange[1] = findIndex(nums, target, false) - 1
+	return targetRange
+}
+
 func main() {
 	nums := []int{5, 7, 7, 8, 8, 9, 10, 11, 12, 12}
-	target := 7
+	target := 8
 	fmt.Println("input Array: ", nums)
-	fmt.Println("Output: ", searchRange(nums, target))
+	fmt.Println("Output: ", searchRange1(nums, target))
 }
