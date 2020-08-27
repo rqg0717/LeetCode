c391be170a2231a85521c17e903742c535b2ccec
James Ren <rqg0717@gmail.com>
2020-08-26 23:29:45 -0500

    Updated 153.

--- Binary Search/153. Find Minimum in Rotated Sorted Array/main.go
+++ Binary Search/153. Find Minimum in Rotated Sorted Array/main.go
@@ -2,23 +2,28 @@ package main
 
 import "fmt"
 
-func search(nums []int, left, right int) int {
-	if left == right {
+func findMin(nums []int) int {
+	if len(nums) == 0 {
+		return -1
+	}
+	left, right := 0, len(nums)-1
+
+	for left+1 < right {
+		mid := left + (right-left)>>1
+		if nums[mid] <= nums[right] {
+			right = mid
+		} else {
+			left = mid
+		}
+	}
+	if nums[left] > nums[right] {
 		return nums[right]
 	}
-	mid := left + (right-left)>>1
-	if nums[left] < nums[mid] {
-		return search(nums, left, mid)
-	}
-	return search(nums, mid+1, right)
-}
-
-func findMin(nums []int) int {
-	return search(nums, 0, len(nums)-1)
+	return nums[left]
 }
 
 func main() {
-	nums := []int{4, 5, 6, 7, 0, 1, 2}
+	nums := []int{2, 3, 4, 5, 6, 7, 0, 1}
 	fmt.Println("input Array: ", nums)
 	fmt.Println("Output: ", findMin(nums))
 }
