002538fb27e2a5032427f0c0418c8bb11d46486a
James Ren <rqg0717@gmail.com>
2020-08-29 13:20:39 -0500

    Updated 744.

--- Strings/744. Find Smallest Letter Greater Than Target/main.go
+++ Strings/744. Find Smallest Letter Greater Than Target/main.go
@@ -15,9 +15,25 @@ func nextGreatestLetter(letters []byte, target byte) byte {
 	return letters[left%len(letters)]
 }
 
+func nextGreatestLetter1(letters []byte, target byte) byte {
+	seen := [26]bool{}
+	for _, ch := range letters {
+		seen[ch-'a'] = true
+	}
+	for {
+		target++
+		if target > 'z' {
+			target = 'a'
+		}
+		if seen[target-'a'] {
+			return target
+		}
+	}
+}
+
 func main() {
 	s := "cfj"
 	letters := []byte(s)
 	target := byte('a')
-	fmt.Println("Output: ", nextGreatestLetter(letters, target))
+	fmt.Println("Output: ", nextGreatestLetter1(letters, target))
 }
