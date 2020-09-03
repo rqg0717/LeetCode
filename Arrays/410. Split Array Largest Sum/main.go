26d7dfbaf719b0a9bc90fcd036a0a6dc3bc7358a
James Ren <rqg0717@gmail.com>
2020-09-02 22:51:37 -0500

    Updated 410.

--- Arrays/410. Split Array Largest Sum/main.go
+++ Arrays/410. Split Array Largest Sum/main.go
@@ -2,7 +2,22 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
 
 func splitArray(nums []int, m int) int {
 	left, right := 0, 0
@@ -37,8 +52,37 @@ func checkSum(nums []int, mid, m, n int) bool {
 	return counter <= m
 }
 
+// minimize the largest sum among these m subarrays
+// dp[i][j]=min{max(dp[k][j−1],sum(k+1,i))}
+func splitArray1(nums []int, m int) int {
+	n := len(nums)
+	sum := make([]int, n+1)
+	// sum(i, j)
+	for i := 0; i < n; i++ {
+		sum[i+1] = sum[i] + nums[i]
+	}
+	dp := make([][]int, n+1)
+	// init
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, m+1)
+		for j := 0; j < len(dp[i]); j++ {
+			dp[i][j] = math.MaxInt32
+		}
+	}
+	// k != 0
+	dp[0][0] = 0
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= min(i, m); j++ {
+			for k := 0; k < i; k++ {
+				dp[i][j] = min(dp[i][j], max(dp[k][j-1], sum[i]-sum[k]))
+			}
+		}
+	}
+	return dp[n][m]
+}
+
 func main() {
 	nums := []int{0, 1, 2, 4, 6, 5, 1}
 	m := 2
-	fmt.Println("Output: ", splitArray(nums, m))
+	fmt.Println("Output: ", splitArray1(nums, m))
 }
