70e99eed1fb53cd6e4d84a38715cc9d7decd9df5
James Ren <rqg0717@gmail.com>
2020-09-07 10:42:32 -0500

    Updated 300.

--- Dynamic Programming/300. Longest Increasing Subsequence/main.go
+++ Dynamic Programming/300. Longest Increasing Subsequence/main.go
@@ -15,16 +15,14 @@ func lengthOfLIS(nums []int) int {
 		return 0
 	}
 	dp := make([]int, n)
-	dp[0] = 1
-	ret := 0
-	for i := 1; i < n; i++ {
-		value := 0
+	ret := 1
+	for i := 0; i < n; i++ {
+		dp[i] = 1
 		for j := 0; j < i; j++ {
-			if nums[j] > nums[i] {
-				value = max(value, dp[j])
+			if nums[j] < nums[i] {
+				dp[i] = max(dp[i], dp[j]+1)
 			}
 		}
-		dp[i] = value + 1
 		ret = max(ret, dp[i])
 	}
 	return ret
