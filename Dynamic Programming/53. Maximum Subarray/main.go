040cd69dbab630d8cf7ba4007cd47c2a400d67e7
James Ren <rqg0717@gmail.com>
2020-09-08 19:56:11 -0500

    Updated 53.

--- Dynamic Programming/53. Maximum Subarray/main.go
+++ Dynamic Programming/53. Maximum Subarray/main.go
@@ -10,10 +10,30 @@ func max(x, y int) int {
 }
 
 func maxSubArray(nums []int) int {
-
+	n := len(nums)
+	if n < 1 {
+		return 0
+	}
+	max := nums[0]
+	if n == 1 {
+		return max
+	}
+	dp := make([]int, n)
+	dp[0] = nums[0]
+	for i := 1; i < n; i++ {
+		if nums[i]+dp[i-1] > nums[i] {
+			dp[i] = nums[i] + dp[i-1]
+		} else {
+			dp[i] = nums[i]
+		}
+		if dp[i] > max {
+			max = dp[i]
+		}
+	}
+	return max
 }
 
 func main() {
-	nums := []int{10, 9, 4, 5, 2, 7, 1, 0, 6}
+	nums := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
 	fmt.Println("Output: ", maxSubArray(nums))
 }
