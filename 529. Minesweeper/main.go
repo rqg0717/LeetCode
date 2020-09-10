d9eae956f3e2a2ba36f52578ac98a7545d611205
James Ren <rqg0717@gmail.com>
2020-09-09 22:54:24 -0500

    Updated 529.

---
+++ 529. Minesweeper/main.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+var dirX = []int{0, 1, 0, -1, 1, 1, -1, -1}
+var dirY = []int{1, 0, -1, 0, 1, -1, 1, -1}
+
+//directions = [(0, 1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (1, 0), (1, -1), (1, 1)]
+
+func updateBoard(board [][]byte, click []int) [][]byte {
+	x, y := click[0], click[1]
+	if board[x][y] == 'M' {
+		board[x][y] = 'X'
+	} else {
+		game(board, x, y)
+	}
+	return board
+}
+
+func game(board [][]byte, x, y int) {
+	count := 0
+	for i := 0; i < 8; i++ {
+		tx, ty := x+dirX[i], y+dirY[i]
+		if tx < 0 || tx >= len(board) || ty < 0 || ty >= len(board[0]) {
+			continue
+		}
+		if board[tx][ty] == 'M' {
+			count++
+		}
+	}
+	if count > 0 {
+		board[x][y] = byte(count + '0')
+	} else {
+		board[x][y] = 'B'
+		for i := 0; i < 8; i++ {
+			tx, ty := x+dirX[i], y+dirY[i]
+			if tx < 0 || tx >= len(board) || ty < 0 || ty >= len(board[0]) || board[tx][ty] != 'E' {
+				continue
+			}
+			game(board, tx, ty)
+		}
+	}
+}
+
+func main() {
+	board := [][]byte{{'E', 'E', 'E', 'E', 'E'},
+		{'E', 'E', 'M', 'E', 'E'},
+		{'E', 'E', 'E', 'E', 'E'},
+		{'E', 'E', 'E', 'E', 'E'}}
+	click := []int{3, 0}
+	board = updateBoard(board, click)
+	fmt.Println("Output: ")
+	for _, array := range board {
+		fmt.Printf("%s\n", string(array))
+	}
+
+}
