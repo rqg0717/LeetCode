69144abfe273b559591af1fad98034536a638388
James Ren <rqg0717@gmail.com>
2020-09-12 10:13:02 -0500

    Updated 1396.

--- Design/1396. Design Underground System/main.go
+++ Design/1396. Design Underground System/main.go
@@ -44,14 +44,61 @@ func (us *UndergroundSystem) GetAverageTime(startStation string, endStation stri
 }
 
 func main() {
-	undergroundSystem := Constructor()
-	undergroundSystem.CheckIn(10, "Leyton", 3)
-	undergroundSystem.CheckOut(10, "Paradise", 8)
-	fmt.Println("Output: ", undergroundSystem.GetAverageTime("Leyton", "Paradise")) // return 5.00000
-	undergroundSystem.CheckIn(5, "Leyton", 10)
-	undergroundSystem.CheckOut(5, "Paradise", 16)
-	fmt.Println("Output: ", undergroundSystem.GetAverageTime("Leyton", "Paradise")) // return 5.50000
-	undergroundSystem.CheckIn(2, "Leyton", 21)
-	undergroundSystem.CheckOut(2, "Paradise", 30)
-	fmt.Println("Output: ", undergroundSystem.GetAverageTime("Leyton", "Paradise")) // return 6.66667
+	undergroundSystem := Constructor1()
+	undergroundSystem.CheckIn1(10, "Leyton", 3)
+	undergroundSystem.CheckOut1(10, "Paradise", 8)
+	fmt.Println("Output: ", undergroundSystem.GetAverageTime1("Leyton", "Paradise")) // return 5.00000
+	undergroundSystem.CheckIn1(5, "Leyton", 10)
+	undergroundSystem.CheckOut1(5, "Paradise", 16)
+	fmt.Println("Output: ", undergroundSystem.GetAverageTime1("Leyton", "Paradise")) // return 5.50000
+	undergroundSystem.CheckIn1(2, "Leyton", 21)
+	undergroundSystem.CheckOut1(2, "Paradise", 30)
+	fmt.Println("Output: ", undergroundSystem.GetAverageTime1("Leyton", "Paradise")) // return 6.66667
+}
+
+// Station ...
+type Station struct {
+	stationName string
+	time        int
+}
+
+// UndergroundSystem1 ...
+type UndergroundSystem1 struct {
+	in  map[int]*Station
+	out map[string][]int
+}
+
+// Constructor1 ...
+func Constructor1() UndergroundSystem1 {
+	us := new(UndergroundSystem1)
+	us.in = map[int]*Station{}
+	us.out = map[string][]int{}
+	return *us
+}
+
+// CheckIn1 A customer with id card equal to id, gets in the station stationName at time t.
+func (us *UndergroundSystem1) CheckIn1(id int, stationName string, t int) {
+	s := &Station{stationName, t}
+	us.in[id] = s
+}
+
+// CheckOut1 A customer with id card equal to id, gets out from the station stationName at time t.
+func (us *UndergroundSystem1) CheckOut1(id int, stationName string, t int) {
+	s := us.in[id]
+	name := s.stationName + stationName
+	time := t - s.time
+	out := us.out[name]
+	out = append(out, time)
+	us.out[name] = out
+}
+
+// GetAverageTime1 Returns the average time to travel between the startStation and the endStation.
+func (us *UndergroundSystem1) GetAverageTime1(startStation string, endStation string) float64 {
+	name := startStation + endStation
+	out := us.out[name]
+	sum := 0
+	for _, t := range out {
+		sum = sum + t
+	}
+	return float64(sum) / float64(len(out))
 }
