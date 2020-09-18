7837515f64f7581df7c47f94bf8bc678484bb125
James Ren <rqg0717@gmail.com>
2020-09-17 22:04:14 -0500

    Updated 1.

--- Arrays/1. two sum/README.md
+++ Arrays/1. two sum/README.md
@@ -1,7 +1,21 @@
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-Example:
+Example 1:
 Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
-return [0, 1].
+return [0,1].
+
+Example 2:
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Example 3:
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+Constraints:
+2 <= nums.length <= 10^5
+-10^9 <= nums[i] <= 10^9
+-10^9 <= target <= 10^9
+Only one valid answer exists.
--- Arrays/1. two sum/main.c
+++ Arrays/1. two sum/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "uthash.h"
 
 /**
  * Note: The returned array must be malloced, assume caller calls free().
@@ -24,10 +24,43 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize){
     return results;
 }
 
+
+struct HASH_TABLE {
+    int key;
+    int value;
+    UT_hash_handle hh;
+};
+
+typedef struct HASH_TABLE *TABLE;
+
+int* twoSum1(int* nums, int numsSize, int target, int* returnSize) {
+    int i = 0, key = 0;
+    TABLE head = NULL, add = NULL, out = NULL;
+    int *results = NULL;
+
+    for( i = 0; i < numsSize; i++ ) {
+        key = target - nums[i];
+        HASH_FIND_INT(head, &key, out);
+        if( out != NULL ) {
+            results = (int*)malloc(sizeof(int) * 2);
+            *returnSize = 2;
+            results[1] = i;
+            results[0] = out->value;
+            return results;
+        }
+        add = (TABLE)malloc(sizeof(struct HASH_TABLE));
+        add->key = nums[i];
+        add->value = i;
+        HASH_ADD_INT(head, key, add);
+    }
+
+    return results;
+}
+
 int main() {
     int nums[4]={2, 7, 11, 15};
     int size = 0;
-    int *results = twoSum(nums, 4, 9, &size);
+    int *results = twoSum1(nums, 4, 9, &size);
     if (size > 0) {
         printf("Output: [%d, %d].", results[0], results[1]);
     } else {
