3636f6b434fbf936374b6de45e2ac0f61605eac8
James Ren <rqg0717@gmail.com>
2020-09-21 16:10:27 -0500

    Updated 141.

---
+++ linked list/141. Linked List Cycle/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+// ListNode definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func hasCycle(head *ListNode) bool {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	node1 := &ListNode{Val: 3}
+	node2 := &ListNode{Val: 2}
+	node3 := &ListNode{Val: 0}
+	node4 := &ListNode{Val: -4}
+
+	node1.Next = node2
+	node2.Next = node3
+	node3.Next = node4
+	node4.Next = node2
+
+	fmt.Println("Output: ", hasCycle(node1))
+}
