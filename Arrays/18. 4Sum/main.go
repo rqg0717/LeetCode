ab5952db1aa6bf9fddc56862d1b250f94f91551f
James Ren <rqg0717@gmail.com>
2020-09-23 11:21:38 -0500

    Updated 18.

--- Arrays/18. 4Sum/main.go
+++ Arrays/18. 4Sum/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 // quick sort
 func sortArray(nums []int) []int {
@@ -60,8 +63,49 @@ func fourSum(nums []int, target int) [][]int {
 	return results
 }
 
+func fourSum1(nums []int, target int) [][]int {
+	n := len(nums)
+	if n < 4 {
+		return nil
+	}
+	results := [][]int{}
+	sort.Ints(nums)
+	//fmt.Println(nums)
+	for i := 0; i < n; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		for j := i + 1; j < n; j++ {
+			if j > i+1 && nums[j] == nums[j-1] {
+				continue
+			}
+			k := j + 1
+			l := n - 1
+			for k < l {
+				sum := nums[i] + nums[k] + nums[j] + nums[l]
+				if sum < target {
+					k++
+				} else if sum > target {
+					l--
+				} else {
+					results = append(results, []int{nums[i], nums[k], nums[j], nums[l]})
+					for k < l && nums[k] == nums[k+1] {
+						k++
+					}
+					for k < l && nums[l] == nums[l-1] {
+						l--
+					}
+					k++
+					l--
+				}
+			}
+		}
+	}
+	return results
+}
+
 func main() {
 	nums := []int{-1, 0, 1, 2, -1, -4, 4, -2}
 	target := 0
-	fmt.Println("Output: ", fourSum(nums, target))
+	fmt.Println("Output: ", fourSum1(nums, target))
 }
