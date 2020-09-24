4182d43d83612de739db2210e4843a35992445a0
James Ren <rqg0717@gmail.com>
2020-09-24 11:12:41 -0500

    Updated 18.

--- Arrays/18. 4Sum/main.go
+++ Arrays/18. 4Sum/main.go
@@ -5,72 +5,13 @@ import (
 	"sort"
 )
 
-// quick sort
-func sortArray(nums []int) []int {
-	if len(nums) < 2 {
-		return nums
-	}
-	if len(nums) == 2 {
-		if nums[0] > nums[1] {
-			return []int{nums[1], nums[0]}
-		}
-		return nums
-	}
-	low, high := []int{}, []int{}
-	for _, n := range nums[1:] {
-		if n < nums[0] {
-			low = append(low, n)
-		} else {
-			high = append(high, n)
-		}
-	}
-	var rerults = append(sortArray(low), nums[0])
-	rerults = append(rerults, sortArray(high)...)
-	return rerults
-}
-
 func fourSum(nums []int, target int) [][]int {
-	n := len(nums)
-	sortedNums := sortArray(nums)
-	results := [][]int{}
-	for i := 0; i < n; i++ {
-		target := 0 - sortedNums[i]
-		left := i + 1
-		right := n - 1
-		if sortedNums[i] > 0 {
-			break
-		}
-		if i == 0 || sortedNums[i] != sortedNums[i-1] {
-			for left < right {
-				if sortedNums[left]+sortedNums[right] == target {
-					results = append(results, []int{sortedNums[i], sortedNums[left], sortedNums[right]})
-					for left < right && sortedNums[left] == sortedNums[left+1] {
-						left++
-					}
-					for left < right && sortedNums[right] == sortedNums[right-1] {
-						right--
-					}
-					left++
-					right--
-				} else if sortedNums[left]+sortedNums[right] < target {
-					left++
-				} else {
-					right--
-				}
-			}
-		}
-	}
-	return results
-}
-
-func fourSum1(nums []int, target int) [][]int {
 	n := len(nums)
 	if n < 4 {
 		return nil
 	}
 	results := [][]int{}
 	sort.Ints(nums)
-	//fmt.Println(nums)
 	for i := 0; i < n; i++ {
 		if i > 0 && nums[i] == nums[i-1] {
 			continue
@@ -107,5 +48,5 @@ func fourSum1(nums []int, target int) [][]int {
 func main() {
 	nums := []int{-1, 0, 1, 2, -1, -4, 4, -2}
 	target := 0
-	fmt.Println("Output: ", fourSum1(nums, target))
+	fmt.Println("Output: ", fourSum(nums, target))
 }
