7997dd06d9dd628417776614f4694cd881ecaeb9
James Ren <rqg0717@gmail.com>
2020-09-24 11:25:56 -0500

    Updated 650.

--- 650. 2 Keys Keyboard/main.go
+++ 650. 2 Keys Keyboard/main.go
@@ -13,6 +13,27 @@ func minSteps(n int) int {
 	return result
 }
 
+func minSteps1(n int) int {
+	if n < 2 {
+		return 0
+	}
+	dp := make([]int, n+1)
+	dp[0] = 0
+	dp[1] = 0
+	dp[2] = 2 // copy and paste
+	for i := 3; i < n+1; i++ {
+		steps := i
+		for j := i / 2; j >= 2; j-- {
+			if i%j == 0 {
+				steps = dp[j] + i/j
+				break
+			}
+		}
+		dp[i] = steps
+	}
+	return dp[n]
+}
+
 func main() {
-	fmt.Println("Output: ", minSteps(3))
+	fmt.Println("Output: ", minSteps1(30))
 }
