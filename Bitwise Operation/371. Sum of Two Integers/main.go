568ec24c23ff8d432bc5e8305662f942c5f0e70c
James Ren <rqg0717@gmail.com>
2020-09-29 18:56:36 -0500

    Updated 371.

--- Bitwise Operation/371. Sum of Two Integers/main.go
+++ Bitwise Operation/371. Sum of Two Integers/main.go
@@ -5,15 +5,24 @@ import "fmt"
 func getSum(a int, b int) int {
 	for b != 0 {
 		sum := a ^ b
-		carry := (a & b) << 1
+		b = (a & b) << 1
 		a = sum
-		b = carry
 	}
 	return a
+}
+
+func getSum1(a int, b int) int {
+	if b == 0 {
+		return a
+	}
+	sum := a ^ b
+	b = a & b << 1
+	a = sum
+	return getSum(a, b)
 }
 
 func main() {
 	a := -2
 	b := 3
-	fmt.Println("Output: ", getSum(a, b))
+	fmt.Println("Output: ", getSum1(a, b))
 }
