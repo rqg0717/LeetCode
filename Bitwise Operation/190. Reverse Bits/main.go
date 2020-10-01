1364ea332cb416626bc3728960fed94369844921
James Ren <rqg0717@gmail.com>
2020-09-30 20:51:31 -0500

    Updated 190.

--- Bitwise Operation/190. Reverse Bits/main.go
+++ Bitwise Operation/190. Reverse Bits/main.go
@@ -13,7 +13,16 @@ func reverseBits(num uint32) uint32 {
 	return result
 }
 
+func reverseBits1(num uint32) uint32 {
+	num = (num >> 16) | (num << 16)
+	num = ((num & 0xff00ff00) >> 8) | ((num & 0x00ff00ff) << 8)
+	num = ((num & 0xf0f0f0f0) >> 4) | ((num & 0x0f0f0f0f) << 4)
+	num = ((num & 0xcccccccc) >> 2) | ((num & 0x33333333) << 2)
+	num = ((num & 0xaaaaaaaa) >> 1) | ((num & 0x55555555) << 1)
+	return num
+}
+
 func main() {
 	num := uint32(0b11111111111111111111111111111101)
-	fmt.Println("Output: ", reverseBits(num))
+	fmt.Println("Output: ", reverseBits1(num))
 }
