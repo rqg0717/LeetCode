5c68a76d0c43ff71de790b17997c826e9e62d336
James Ren <rqg0717@gmail.com>
2020-10-01 18:06:38 -0500

    Updated 389.

--- Bitwise Operation/389. Find the Difference/main.go
+++ Bitwise Operation/389. Find the Difference/main.go
@@ -33,8 +33,25 @@ func findTheDifference2(s string, t string) byte {
 	return byte(result)
 }
 
+func findTheDifference3(s string, t string) byte {
+	result := byte(0)
+	m := make(map[byte]int)
+	for ch := range t {
+		m[t[ch]]++
+	}
+	for ch := range s {
+		m[s[ch]]--
+	}
+	for ch, count := range m {
+		if count == 1 {
+			result = ch
+		}
+	}
+	return result
+}
+
 func main() {
 	s := "abcd"
 	t := "abcde"
-	fmt.Println("Output: ", string(findTheDifference1(s, t)))
+	fmt.Println("Output: ", string(findTheDifference3(s, t)))
 }
