dcae24c5eaeba650ccb4ad23adfdc361fdfce2f0
James Ren <rqg0717@gmail.com>
2020-10-05 22:59:13 -0500

    Updated 268.

--- Bitwise Operation/268. Missing Number/main.go
+++ Bitwise Operation/268. Missing Number/main.go
@@ -12,6 +12,16 @@ func missingNumber(nums []int) int {
 	return num ^ len(nums)
 }
 
+func missingNumber1(nums []int) int {
+	n := len(nums)
+	sum := n * (n + 1) / 2
+	result := 0
+	for _, val := range nums {
+		result += val
+	}
+	return sum - result
+}
+
 func main() {
 	nums := []int{9, 6, 4, 2, 3, 5, 7, 0, 1}
 	fmt.Println("Output: ", missingNumber1(nums))
