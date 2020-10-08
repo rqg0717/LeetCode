ff0bd6354d5ee8588daa97a873ba2ac01ae81452
James Ren <rqg0717@gmail.com>
2020-10-08 17:25:54 -0500

    Updated 1442.

--- Bitwise Operation/1442. Count Triplets That Can Form Two Arrays of Equal XOR/main.go
+++ Bitwise Operation/1442. Count Triplets That Can Form Two Arrays of Equal XOR/main.go
@@ -21,7 +21,24 @@ func countTriplets(arr []int) int {
 	return count
 }
 
+func countTriplets1(arr []int) int {
+	m, tmp, count := map[int][]int{}, 0, 0
+	m[0] = []int{-1}
+	for k, val := range arr {
+		tmp ^= val
+		if j, ok := m[tmp]; !ok {
+			m[tmp] = []int{k}
+		} else {
+			for _, i := range j {
+				count += k - i - 1
+			}
+			m[tmp] = append(m[tmp], k)
+		}
+	}
+	return count
+}
+
 func main() {
 	arr := []int{2, 3, 1, 6, 7}
-	fmt.Println("Output: ", countTriplets(arr))
+	fmt.Println("Output: ", countTriplets1(arr))
 }
