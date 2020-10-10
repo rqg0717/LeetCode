d37c26566c8a6c2c2f3d2f46ef6a5a5bb51ae0e6
James Ren <rqg0717@gmail.com>
2020-10-09 21:12:44 -0500

    Added 89.

---
+++ Bitwise Operation/1238. Circular Permutation in Binary Representation/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func circularPermutation(n int, start int) []int {
+    
+}
+
+func main() {
+	n:= 2
+	start:=3
+	fmt.Println("Output: ", circularPermutation(n, start))
+}
