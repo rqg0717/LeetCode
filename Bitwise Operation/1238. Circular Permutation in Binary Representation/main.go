d2eafda2cdfd53e63763b7f22beda7fb96453d74
James Ren <rqg0717@gmail.com>
2020-10-09 21:37:43 -0500

    Updated 1238.

--- Bitwise Operation/1238. Circular Permutation in Binary Representation/main.go
+++ Bitwise Operation/1238. Circular Permutation in Binary Representation/main.go
@@ -5,11 +5,19 @@ import (
 )
 
 func circularPermutation(n int, start int) []int {
-    
+	results := []int{start}
+	count := 1
+	for i := 0; i < n; i++ {
+		for j := count - 1; j >= 0; j-- {
+			results = append(results, results[j]^count)
+		}
+		count <<= 1
+	}
+	return results
 }
 
 func main() {
-	n:= 2
-	start:=3
+	n := 2
+	start := 3
 	fmt.Println("Output: ", circularPermutation(n, start))
 }
