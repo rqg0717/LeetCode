259dc3ecbae8095ebb943874a27a6584241cddf7
James Ren <rqg0717@gmail.com>
2020-10-11 10:58:13 -0500

    Updated 318.

--- Bitwise Operation/318. Maximum Product of Word Lengths/main.go
+++ Bitwise Operation/318. Maximum Product of Word Lengths/main.go
@@ -23,12 +23,13 @@ func hasCommon(w1, w2 string) bool {
 
 func maxProduct(words []string) int {
 	n := len(words)
-	if n < 1 {
+	if n <= 1 {
 		return 0
 	}
 	result := 0
 	for i := 0; i < n; i++ {
 		for j := i + 1; j < n; j++ {
+			// if !strings.ContainsAny(words[i], words[j]) { // slow
 			if !hasCommon(words[i], words[j]) {
 				result = max(result, len(words[i])*len(words[j]))
 			}
@@ -38,7 +39,29 @@ func maxProduct(words []string) int {
 	return result
 }
 
+func maxProduct1(words []string) int {
+	n := len(words)
+	if n <= 1 {
+		return 0
+	}
+	result := 0
+	m := make([]int, n)
+	for i := 0; i < n; i++ {
+		for _, ch := range words[i] {
+			m[i] |= 1 << (ch - 'a')
+		}
+	}
+	for i := 0; i < n; i++ {
+		for j := i + 1; j < n; j++ {
+			if m[i]&m[j] == 0 {
+				result = max(result, len(words[i])*len(words[j]))
+			}
+		}
+	}
+	return result
+}
+
 func main() {
 	words := []string{"abcw", "baz", "foo", "bar", "xtfn", "abcdef"}
-	fmt.Println("Output: ", maxProduct(words))
+	fmt.Println("Output: ", maxProduct1(words))
 }
