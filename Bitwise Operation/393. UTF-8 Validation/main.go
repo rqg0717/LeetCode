b64c7ac5c20f498baa5305af2021568f40ade48a
James Ren <rqg0717@gmail.com>
2020-10-10 22:16:45 -0500

    Updated 393.

--- Bitwise Operation/393. UTF-8 Validation/main.go
+++ Bitwise Operation/393. UTF-8 Validation/main.go
@@ -5,8 +5,12 @@ import (
 )
 
 func validUtf8(data []int) bool {
+	n := len(data)
+	if n == 0 {
+		return false
+	}
 	bit := 0
-	for i := 0; i < len(data); i++ {
+	for i := 0; i < n; i++ {
 		mask1, mask2 := 1<<7, 1<<6
 		if bit != 0 {
 			if !((data[i]&mask1 != 0) && (data[i]&mask2) == 0) {
@@ -31,7 +35,52 @@ func validUtf8(data []int) bool {
 	return bit == 0
 }
 
+func length(num int) int {
+	// 0xxxxxxx
+	if num>>7 == 0 {
+		return 0
+	}
+	// 110xxxxx 10xxxxxx
+	if num>>5 == 6 {
+
+		return 1
+	}
+	// 1110xxxx 10xxxxxx 10xxxxxx
+	if num>>4 == 14 {
+		return 2
+	}
+	// 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+	if num>>3 == 30 {
+		return 3
+	}
+	return -1
+}
+
+func validUtf8a(data []int) bool {
+	n := len(data)
+	if n == 0 {
+		return false
+	}
+	l := 0
+	for i := 0; i < n; i++ {
+		if l == 0 {
+			l = length(data[i])
+		} else {
+			// 10xxxxxx
+			if data[i]&(3<<6) != 1<<7 {
+				return false
+			}
+			l--
+		}
+		if l == -1 {
+			return false
+		}
+	}
+	return l == 0
+
+}
+
 func main() {
 	data := []int{235, 140, 4}
-	fmt.Println("Output: ", validUtf8(data))
+	fmt.Println("Output: ", validUtf8a(data))
 }
