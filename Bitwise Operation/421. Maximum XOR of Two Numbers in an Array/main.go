17615ff234a288a721fa3d122313974f7bb57b46
James Ren <rqg0717@gmail.com>
2020-10-13 22:29:34 -0500

    Updated 421.

---
+++ Bitwise Operation/421. Maximum XOR of Two Numbers in an Array/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func findMaximumXOR(nums []int) int {
+	result := 0
+	mask := 0
+	for i := 31; i >= 0; i-- {
+		mask = mask | (1 << i)
+		m := make(map[int]bool)
+		for _, num := range nums {
+			m[num&mask] = true
+		}
+		tmp := (1 << i) | result
+		for j := range m {
+			if _, ok := m[tmp^j]; ok {
+				result = tmp
+				break
+			}
+		}
+	}
+	return result
+}
+
+func main() {
+	nums := []int{14, 70, 53, 83, 49, 91, 36, 80, 92, 51, 66, 70}
+	fmt.Println("Output: ", findMaximumXOR(nums))
+}
