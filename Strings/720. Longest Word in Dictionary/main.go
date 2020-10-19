d2f51e1b90156ee77c8f88c25cf3fc2fe9f43eae
James Ren <rqg0717@gmail.com>
2020-10-18 20:12:33 -0500

    Added 208.

---
+++ Strings/720. Longest Word in Dictionary/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func longestWord(words []string) string {
+// use trie
+}
+
+func main() {
+	words = []string{"a", "banana", "app", "appl", "ap", "apply", "apple"}
+	fmt.Println("Output: ", longestWord(words))
+}
