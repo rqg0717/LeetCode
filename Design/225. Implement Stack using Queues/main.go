a13ca2a24522375d16629bfbcb7095b0fc92a4ab
James Ren <rqg0717@gmail.com>
2020-10-21 18:59:16 -0500

    Updated 225.

---
+++ Design/225. Implement Stack using Queues/main.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+// MyStack  is a struct
+type MyStack struct {
+	queue []int
+}
+
+// Constructor init the stack
+func Constructor() MyStack {
+	return MyStack{queue: []int{}}
+}
+
+// Push adds an item
+func (s *MyStack) Push(x int) {
+	n := len(s.queue)
+	s.queue = append(s.queue, x)
+	for ; n > 0; n-- {
+		s.queue = append(s.queue, s.queue[0])
+		s.queue = s.queue[1:]
+	}
+}
+
+// Top returns the first tiem
+func (s *MyStack) Top() int {
+	return s.queue[0]
+}
+
+// Pop removes the last item
+func (s *MyStack) Pop() int {
+	item := s.queue[0]
+	s.queue = s.queue[1:]
+	return item
+}
+
+// Empty returns if the stack is empty
+func (s *MyStack) Empty() bool {
+	return len(s.queue) == 0
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Push(x);
+ * param_2 := obj.Pop();
+ * param_3 := obj.Top();
+ * param_4 := obj.Empty();
+ */
+
+func main() {
+	stack := Constructor()
+	stack.Push(-2)
+	stack.Push(0)
+	stack.Push(-3)
+	fmt.Println(stack.Top())   // returns -3
+	fmt.Println(stack.Pop())   // returns -3
+	fmt.Println(stack.Empty()) // returns false
+}
