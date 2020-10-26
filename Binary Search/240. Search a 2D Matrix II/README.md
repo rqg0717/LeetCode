b07d759b68006385036b553d10b3de5734da2d89
James Ren <rqg0717@gmail.com>
2020-10-25 19:27:34 -0500

    Updated 240.

--- Binary Search/240. Search a 2D Matrix II/README.md
+++ Binary Search/240. Search a 2D Matrix II/README.md
@@ -2,17 +2,16 @@ Write an efficient algorithm that searches for a value in an m x n matrix. This 
 
 Integers in each row are sorted in ascending from left to right.
 Integers in each column are sorted in ascending from top to bottom.
+
 Example:
-
 Consider the following matrix:
-
 [
-  [1,   4,  7, 11, 15],
-  [2,   5,  8, 12, 19],
-  [3,   6,  9, 16, 22],
-  [10, 13, 14, 17, 24],
-  [18, 21, 23, 26, 30]
+  [1,   4,  7, 11, 15, 17],
+  [2,   5,  8, 12, 19, 20],
+  [3,   6,  9, 16, 22, 23],
+  [10, 13, 14, 17, 24, 25],
+  [18, 21, 23, 26, 28, 30]
 ]
 Given target = 5, return true.
 
-Given target = 20, return false.
+Given target = 27, return false.
---
+++ Binary Search/240. Search a 2D Matrix II/main.go
@@ -0,0 +1,74 @@
+package main
+
+import "fmt"
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func binarySearch(matrix [][]int, target int, start int, isVertical bool) bool {
+	left := start
+	right := 0
+	if isVertical {
+		right = len(matrix[0]) - 1
+	} else {
+		right = len(matrix) - 1
+	}
+	for right >= left {
+		mid := left + (right-left)>>2
+		if isVertical {
+			if matrix[start][mid] < target {
+				left = mid + 1
+			} else if matrix[start][mid] > target {
+				right = mid - 1
+			} else {
+				return true
+			}
+		} else {
+			if matrix[mid][start] < target {
+				left = mid + 1
+			} else if matrix[mid][start] > target {
+				right = mid - 1
+			} else {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func searchMatrix(matrix [][]int, target int) bool {
+	m, n := len(matrix), len(matrix[0])
+	if m == 0 || n == 0 {
+		return false
+	}
+	shorter := min(m, n)
+	isFound := false
+	for i := 0; i < shorter; i++ {
+		isFound = binarySearch(matrix, target, i, true)
+		if isFound {
+			break
+		}
+		isFound = binarySearch(matrix, target, i, false)
+		if isFound {
+			break
+		}
+	}
+	return isFound
+}
+
+func main() {
+	target := 16
+	matrix := [][]int{
+		{1, 4, 7, 11, 15, 17},
+		{2, 5, 8, 12, 19, 20},
+		{3, 6, 9, 16, 22, 23},
+		{10, 13, 14, 17, 24, 25},
+		{18, 21, 23, 26, 28, 30}}
+
+	fmt.Println("input Matrix: ", matrix)
+	fmt.Println("Output: ", searchMatrix(matrix, target))
+}
