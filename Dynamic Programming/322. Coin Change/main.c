6e98ae5b870c6618df25e7e8b9cf296c1bbaf411
James Ren <rqg0717@gmail.com>
2020-11-20 17:40:16 -0600

    Updated 322.

--- Dynamic Programming/322. Coin Change/main.c
+++ Dynamic Programming/322. Coin Change/main.c
@@ -1,17 +1,46 @@
-int coinChange(int* coins, int coinsSize, int amount)
+#include <stdio.h>
+#include <stdlib.h>
+
+int getMIN(int x, int y)
 {
-	int i = 0, ret = -1;
-    int * opt = (int *)malloc(sizeof(int) * (amount + 1));
-	opt[0] = 0;
-	for (i = 1; i <= amount; i++) {
-        opt[i] = amount + 1;
-		for (int j = 0; j < coinsSize; j++) {
-			if (i >= coins[j]) {
-				opt[i] = min(opt[i - coins[j]] + 1, opt[i]);
+	int ret = x < y ? x : y;
+	return ret;
+}
+
+int coinChange(int *coins, int coinsSize, int amount)
+{
+	int i = 0, result = -1;
+	int *dp = (int *)malloc(sizeof(int) * (amount + 1));
+	dp[0] = 0;
+	for (i = 1; i <= amount; i++)
+	{
+		dp[i] = amount + 1;
+		for (int j = 0; j < coinsSize; j++)
+		{
+			if (i >= coins[j])
+			{
+				dp[i] = getMIN(dp[i - coins[j]] + 1, dp[i]);
 			}
 		}
 	}
-    ret = opt[amount] > amount ? -1 : opt[amount]
-    free(opt);
-	return ret;
+	result = dp[amount] > amount ? -1 : dp[amount];
+	free(dp);
+	return result;
+}
+
+int main()
+{
+	int coins[3] = {1, 2, 5};
+	int coinsSize = 3;
+	int amount = 11;
+	int result = coinChange(coins, coinsSize, amount);
+	if (result > 0)
+	{
+		printf("Output: [%d].", result);
+	}
+	else
+	{
+		printf("Output: NO RESULT.");
+	}
+	return 0;
 }
